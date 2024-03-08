3cdc9deea2600fbcc7be8c46dd897809216e333a
HectorYang <how30013@hotmail.com>
2024-03-09 02:42:29 +0800

    feat: memorization for menu page and item component

--- app/menu/item.tsx
+++ app/menu/item.tsx
@@ -1,9 +1,27 @@
 import React, { memo } from 'react';
 import { FoodItem } from '../types';
 
-const Item = ({ name, price }: FoodItem) => {
+export interface ItemProps extends FoodItem {
+  addToCart: () => void;
+}
+
+const Item = ({ id, name, price, addToCart }: ItemProps) => {
+  console.log(`render: ${id}`);
   return (
-    <li className='capitalize cursor-pointer hover:bg-gray-200'>{name}</li>
+    <li className='capitalize flex justify-between mb-1'>
+      <div className='flex-1 whitespace-nowrap'>
+        <div className='font-semibold text-base'>{name}</div>
+        <div className='text-sm'>{`$${price}`}</div>
+      </div>
+      <div>
+        <button
+          className='bg-sky-600 text-white px-3 py-1 rounded-md hover:bg-sky-800'
+          onClick={addToCart}
+        >
+          Add to cart
+        </button>
+      </div>
+    </li>
   );
 };
 
--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,25 +1,49 @@
-import React, { Fragment, useState } from 'react';
-import { FOOD_LIST } from '../constants';
+'use client';
+import React, { Fragment, useCallback } from 'react';
+import Link from 'next/link';
 import Item from './item';
+import { MENU } from '../constants';
+import { useFoodState } from '../store';
+import { useShallow } from 'zustand/react/shallow';
+import { LinkButton } from '../components';
 
 const Menu = () => {
+  const updateItem = useFoodState(state => state.updateItem);
+
+  const addToCart = useCallback((id: number) => {
+    return () => updateItem(id, 1);
+  }, []);
+
+  // console.log(cartItemLength);
+
   return (
-    <div className='w-screen h-screen flex justify-center items-center'>
-      <ul className='w-80'>
-        {FOOD_LIST.map(food => {
-          return (
-            <Fragment key={food.category}>
-              <li className='capitalize font-bold'>{food.category}</li>
-              <ul className='indent-5 mb-2'>
-                {food.items.map(item => (
-                  <Item {...item} />
-                ))}
-              </ul>
-              <div className='w-full h-px bg-gray-400' />
-            </Fragment>
-          );
-        })}
-      </ul>
+    <div className='w-screen h-screen flex justify-center items-center flex-col'>
+      <div className='w-80 flex justify-center'>
+        <ul className='w-full'>
+          {MENU.map(food => {
+            return (
+              <Fragment key={food.category.id}>
+                <li className='capitalize font-bold text-lg'>
+                  {food.category.name}
+                </li>
+                <ul className='indent-5 mb-2'>
+                  {food.items.map(item => (
+                    <Item
+                      key={item.id}
+                      {...item}
+                      addToCart={addToCart(item.id)}
+                    />
+                  ))}
+                </ul>
+                <div className='w-full h-px bg-gray-400' />
+              </Fragment>
+            );
+          })}
+        </ul>
+      </div>
+      <div className='mt-3'>
+        <LinkButton href='/cart'>Go to cart</LinkButton>
+      </div>
     </div>
   );
 };
