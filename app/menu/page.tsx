814c92771b1f552b66e287824ed536869880d34c
HectorYang <how30013@hotmail.com>
2024-03-12 16:29:12 +0800

    refactor: add Container component, extract zustand function from store

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,53 +1,44 @@
 'use client';
-import React, { Fragment, useCallback } from 'react';
-import Link from 'next/link';
+import React, { Fragment } from 'react';
 import Item from './item';
 import { MENU } from '../constants';
-import { useFoodState } from '../store';
+import { useFoodState, updateItem } from '../store';
+import { LinkButton, Container } from '../components';
 import { useShallow } from 'zustand/react/shallow';
-import { LinkButton } from '../components';
 
 const Menu = () => {
-  const updateItem = useFoodState(state => state.updateItem);
-
-  const addToCart = useCallback((id: number) => {
-    return () => updateItem(id, 1);
-  }, []);
-
-  // console.log(cartItemLength);
+  const itemCount = useFoodState(useShallow(state => state.cartItems.length));
 
   return (
-    <div className='w-screen h-screen flex justify-center items-center flex-col'>
-      <div className='w-80 flex justify-center'>
-        <ul className='w-full'>
-          {MENU.map(food => {
-            return (
-              <Fragment key={food.category.id}>
-                <li
-                  data-testid={`category-${food.category.id}`}
-                  className='capitalize font-bold text-lg'
-                >
-                  {food.category.name}
-                </li>
-                <ul className='indent-5 mb-2'>
-                  {food.items.map(item => (
-                    <Item
-                      key={item.id}
-                      {...item}
-                      addToCart={addToCart(item.id)}
-                    />
-                  ))}
-                </ul>
-                <div className='w-full h-px bg-gray-400' />
-              </Fragment>
-            );
-          })}
-        </ul>
-      </div>
+    <Container>
+      <ul className='w-full'>
+        {MENU.map(food => {
+          return (
+            <Fragment key={food.category.id}>
+              <li
+                data-testid={`category-${food.category.id}`}
+                className='capitalize font-bold text-lg'
+              >
+                {food.category.name}
+              </li>
+              <ul className='indent-5 mb-2'>
+                {food.items.map(item => (
+                  <Item
+                    key={item.id}
+                    {...item}
+                    addToCart={() => updateItem(item.id, 1)}
+                  />
+                ))}
+              </ul>
+              <div className='w-full h-px bg-gray-400' />
+            </Fragment>
+          );
+        })}
+      </ul>
       <div className='mt-3'>
-        <LinkButton href='/cart'>Go to cart</LinkButton>
+        <LinkButton href='/cart'>{`Go to cart (${itemCount})`}</LinkButton>
       </div>
-    </div>
+    </Container>
   );
 };
 
--- app/store/food.ts
+++ app/store/food.ts
@@ -12,7 +12,6 @@ export interface FoodState {
   foodDic: Record<number, Omit<CartItem, 'quantity'>>;
   cartItems: CartItem[];
   history: CartItem[][];
-  updateItem: (id: number, delta: number) => void;
   submit: () => void;
 }
 
@@ -25,47 +24,6 @@ export const useFoodState = create<FoodState>()(set => ({
   }, {} as Record<number, Omit<CartItem, 'quantity'>>),
   cartItems: [],
   history: [],
-  updateItem: (id, delta) =>
-    set(state => {
-      const { cartItems } = state;
-      const index = cartItems.findIndex(e => e.id === id);
-      if (index > -1) {
-        const data = cartItems[index];
-        const { quantity } = data;
-        const newQty = quantity + delta;
-
-        if (newQty <= 0) {
-          // Remove from the cart
-          return {
-            cartItems: [
-              ...cartItems.slice(0, index),
-              ...cartItems.slice(index + 1)
-            ]
-          };
-        }
-
-        return {
-          cartItems: [
-            ...cartItems.slice(0, index),
-            { ...data, quantity: newQty },
-            ...cartItems.slice(index + 1)
-          ]
-        };
-      }
-
-      if (delta < 0) {
-        // Not in the list with negative qty
-        return state;
-      }
-
-      // Add to list
-      return {
-        cartItems: [
-          ...state.cartItems,
-          { ...state.foodDic[id], quantity: delta }
-        ]
-      };
-    }),
   submit: () =>
     set(state => {
       return {
@@ -74,3 +32,43 @@ export const useFoodState = create<FoodState>()(set => ({
       };
     })
 }));
+
+export const updateItem = (id: number, by: number) => {
+  useFoodState.setState(state => {
+    const { cartItems } = state;
+    const index = cartItems.findIndex(e => e.id === id);
+    if (index > -1) {
+      const data = cartItems[index];
+      const { quantity } = data;
+      const newQty = quantity + by;
+
+      if (newQty <= 0) {
+        // Remove from the cart
+        return {
+          cartItems: [
+            ...cartItems.slice(0, index),
+            ...cartItems.slice(index + 1)
+          ]
+        };
+      }
+
+      return {
+        cartItems: [
+          ...cartItems.slice(0, index),
+          { ...data, quantity: newQty },
+          ...cartItems.slice(index + 1)
+        ]
+      };
+    }
+
+    if (by < 0) {
+      // Not in the list with negative qty
+      return state;
+    }
+
+    // Add to list
+    return {
+      cartItems: [...state.cartItems, { ...state.foodDic[id], quantity: by }]
+    };
+  });
+};
